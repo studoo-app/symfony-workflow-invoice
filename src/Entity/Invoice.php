2419cfe0b0b57737aae32df7ab92a9b3d947a93d
JPechberty <Ninposhine313>
2023-10-16 23:41:34 +0200

    Build Interface prototype

---
+++ src/Entity/Invoice.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\InvoiceRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: InvoiceRepository::class)]
+class Invoice
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column()]
+    private ?\DateTime $udpatedAt = null;
+
+    #[ORM\Column()]
+    private ?\DateTime $date = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $client = null;
+
+    #[ORM\Column]
+    private ?int $total = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $status = null;
+
+    public function __construct()
+    {
+        $this->udpatedAt = new \DateTime('now', new \DateTimeZone('Europe/Paris'));
+        $this->status = "Générée";
+    }
+
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getUdpatedAt(): ?\DateTime
+    {
+        return $this->udpatedAt;
+    }
+
+    public function setUdpatedAt(\DateTime $udpatedAt): static
+    {
+        $this->udpatedAt = $udpatedAt;
+
+        return $this;
+    }
+
+    public function getDate(): ?\DateTime
+    {
+        return $this->date;
+    }
+
+    public function setDate(\DateTime $date): static
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+    public function getClient(): ?string
+    {
+        return $this->client;
+    }
+
+    public function setClient(string $client): static
+    {
+        $this->client = $client;
+
+        return $this;
+    }
+
+    public function getTotal(): ?int
+    {
+        return $this->total;
+    }
+
+    public function setTotal(int $total): static
+    {
+        $this->total = $total;
+
+        return $this;
+    }
+
+    public function getStatus(): ?string
+    {
+        return $this->status;
+    }
+
+    public function setStatus(string $status): static
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+}
